299eb51a28069d62139b675474a261e05e60ae4a
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-11-03 11:01:16 -0600

    getting started

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const SchemeRouter = require('./schemes/scheme-router.js');
 
 const server = express();
-
+// sanity check
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
